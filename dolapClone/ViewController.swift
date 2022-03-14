da0ebb8fb959e57bb4f4cdc617a6c921e977cb42
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-03-14 03:06:01 +0300

    top components setting

--- dolapClone/ViewController.swift
+++ dolapClone/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var upView: UIStackView!
+    @IBOutlet weak var sizeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setIcon()
+        //setupButton(buton: sizeButton)
+        //setupButton(buton: xbtn)
+        //setupButton(buton: zbtn)
+        
+        upView.layer.borderWidth = 0.3
+        upView.layer.borderColor = UIColor.lightGray.cgColor
+        
     }
-
-
+    func setIcon(){
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 38, height: 38))
+        imageView.contentMode = .scaleAspectFit
+        let image = UIImage(named: "logo")
+        imageView.image = image
+        self.navigationItem.titleView = imageView
+    }
+    
+    func setupButton(button: UIButton) {
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 0.3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+    }
 }
+
+
 
